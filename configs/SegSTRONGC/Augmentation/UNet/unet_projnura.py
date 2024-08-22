823e916666a8bc09c76009fadbea8e8a14c63eca
ATATC <futerry@outlook.com>
2024-08-22 14:06:28 -0400

    Routine updates.

--- configs/SegSTRONGC/Augmentation/UNet/unet_projnura.py
+++ configs/SegSTRONGC/Augmentation/UNet/unet_projnura.py
@@ -4,7 +4,7 @@ from torch.nn import BCELoss
 from torch.optim import SGD
 from torch.optim.lr_scheduler import StepLR
 
-from augmentation import wrap, Smoke, RandomRotate
+from augmentation import wrap, Smoke
 from datasets.transformation.autoaugment import AutoAugment
 
 transform = T.Compose([
@@ -22,7 +22,7 @@ class UNetProjNura(object):
             set_indices=[3, 4, 5, 7, 8],
             subset_indices=[[0, 2], [0, 1, 2], [0, 2], [0, 1], [1, 2]],
             domains=['regular'],
-            image_transforms=[transform, lambda x : x.to(torch.uint8), AutoAugment, wrap(Smoke(p=0.5)), lambda x: x.to(torch.float)],
+            image_transforms=[transform, lambda x : x.to(torch.uint8), wrap(Smoke(p=0.5)), AutoAugment, lambda x: x.to(torch.float)],
             gt_transforms=[True, False, False, False, False]
         )
     )
