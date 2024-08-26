f25d5d6d78882297f588416e1f9ae41d994ff146
ATATC <futerry@outlook.com>
2024-08-26 19:12:45 -0400

    Updated augmentation.

--- configs/SegSTRONGC/Augmentation/UNet/unet_projnura.py
+++ configs/SegSTRONGC/Augmentation/UNet/unet_projnura.py
@@ -5,6 +5,7 @@ from torch.optim import SGD
 from torch.optim.lr_scheduler import StepLR
 
 from augmentation import wrap, Blood
+from datasets.transformation.autoaugment import AutoAugment
 
 transform = T.Compose([
     T.ToTensor(),
@@ -21,7 +22,7 @@ class UNetProjNura(object):
             set_indices=[3, 4, 5, 7, 8],
             subset_indices=[[0, 2], [0, 1, 2], [0, 2], [0, 1], [1, 2]],
             domains=['regular'],
-            image_transforms=[transform, wrap(Blood(10, p=0.5)), lambda x : x.to(torch.uint8), lambda x: x.to(torch.float)],
+            image_transforms=[transform, AutoAugment, wrap(Blood(10, p=0.5)), lambda x : x.to(torch.uint8), lambda x: x.to(torch.float)],
             gt_transforms=[True, False, False, False, False]
         )
     )
